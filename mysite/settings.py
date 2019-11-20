e69d7ca0e4a53e3605957107f80cf676fa86e551
Dima1329 <57827956+Dima1329@users.noreply.github.com>
2019-11-20 10:38:21 +0300

    Update settings.py
    Try to delete a mistake

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,3 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+path =  "/home/Dima1329/my-first-blog"
+if path not in sys.path:
+    sys.path.insert(0,  path)
+
+os.environ["DJANGO_SETTINGS_MODULE"]  =  "my-first-blog.mysite.settings"
