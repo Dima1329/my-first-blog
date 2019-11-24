97104c2209d75c2759e7740ff93f255249164e01
Dima1329 <peyton.b1329@gmail.com>
2019-11-24 10:07:50 +0300

    ОТредоктировал настройки

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import sys
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -111,8 +113,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+path =  "/home/Dima1329/my-first-blog"
+if path not in sys.path:
+    sys.path.insert(0,  path)
 
-
+os.environ["DJANGO_SETTINGS_MODULE"]  =  "my-first-blog.mysite.settings"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
